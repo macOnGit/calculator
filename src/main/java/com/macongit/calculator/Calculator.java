04a23cbf6db40ae6a49166874bc366dfa34e7ad8
cmac <>
2024-06-04 17:24:43 -0400

    Add sum logic, controller, and unit test

---
+++ src/main/java/com/macongit/calculator/Calculator.java
@@ -0,0 +1,10 @@
+package com.macongit.calculator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+}
